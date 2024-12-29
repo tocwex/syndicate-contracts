a6f59ea583aac6cb68baaead3da935c1ed9c3a92
~sarlev-sarsen <tlatfcp@protonmail.com>
2024-12-28 21:03:40 -0800

    basic working deployment script

---
+++ script/DeploySyndicate.s.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPLv3
+
+pragma solidity ^0.8.19;
+
+// TODO
+// Deployment script
+// include setup of initial contracts
+// might need to get into deploying the ERC6551 and Azimuth contracts onto my testnets if/when we get more complicated about the minting permissions
+
+import {Script} from "@forge-std/Script.sol";
+import {SyndicateToken} from "../src/SyndicateToken.sol";
+
+contract DeploySyndicate is Script {
+    struct DeploymentParams {
+        address owner;
+        uint256 initialSupply;
+        uint256 maxSupply;
+        string name;
+        string symbol;
+    }
+
+    DeploymentParams params;
+
+    function setUp() public {
+        params = DeploymentParams({
+            owner: msg.sender,
+            initialSupply: 100 * 10 ** 18,
+            maxSupply: 1000 * 10 ** 18,
+            name: "Syndicate Token",
+            symbol: "SYN"
+        });
+    }
+
+    function run() external returns (SyndicateToken) {
+        vm.startBroadcast();
+        SyndicateToken syndicateToken = new SyndicateToken(
+            params.owner,
+            params.initialSupply,
+            params.maxSupply,
+            params.name,
+            params.symbol
+        );
+        vm.stopBroadcast();
+        return syndicateToken;
+    }
+}
