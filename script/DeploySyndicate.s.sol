5f7b97cbb03f66005a9cbabc5c2cceb85725b4a9
~sarlev-sarsen <tlatfcp@protonmail.com>
2025-01-06 19:06:29 -0800

    fix imports with new filenames

--- script/DeploySyndicate.s.sol
+++ script/DeploySyndicate.s.sol
@@ -8,17 +8,17 @@ pragma solidity ^0.8.19;
 // might need to get into deploying the ERC6551 and Azimuth contracts onto my testnets if/when we get more complicated about the minting permissions
 
 import {Script} from "@forge-std/Script.sol";
-import {SyndicateToken} from "../src/SyndicateToken.sol";
-import {SyndicateDeployer} from "../src/SyndicateDeployer.sol";
+import {SyndicateTokenV1} from "../src/SyndicateTokenV1.sol";
+import {SyndicateDeployerV1} from "../src/SyndicateDeployerV1.sol";
 
 contract DeploySyndicate is Script {
     function setUp() public {}
 
-    function run() external returns (SyndicateDeployer) {
+    function run() external returns (SyndicateDeployerV1) {
         vm.startBroadcast();
-        SyndicateDeployer syndicateDeployer = new SyndicateDeployer();
+        SyndicateDeployerV1 syndicateDeployerV1 = new SyndicateDeployerV1();
         vm.stopBroadcast();
-        return syndicateDeployer;
+        return syndicateDeployerV1;
     }
     // struct DeploymentParams {
     //     address owner;
--- test/unit/SyndicateTokenTest.t.sol
+++ test/unit/SyndicateTokenTest.t.sol
@@ -6,12 +6,12 @@ pragma solidity ^0.8.19;
 
 import {Test} from "@forge-std/Test.sol";
 import {console} from "@forge-std/console.sol";
-import {SyndicateToken} from "../../src/SyndicateToken.sol";
-import {SyndicateDeployer} from "../../src/SyndicateDeployer.sol";
+import {SyndicateTokenV1} from "../../src/SyndicateTokenV1.sol";
+import {SyndicateDeployerV1} from "../../src/SyndicateDeployerV1.sol";
 
 contract SyndicateTokenTest is Test {
-    SyndicateToken public syndicateToken;
-    SyndicateDeployer public syndicateDeployer;
+    SyndicateTokenV1 public syndicateTokenV1;
+    SyndicateDeployerV1 public syndicateDeployerV1;
 
     // Environment Variables
     address owner = makeAddr("bob");
@@ -23,11 +23,11 @@ contract SyndicateTokenTest is Test {
     address recipient = makeAddr("alice");
 
     function setUp() public {
-        syndicateDeployer = new SyndicateDeployer();
-        console.log("Deployer launched at: ", address(syndicateDeployer));
-        vm.startPrank(address(syndicateDeployer));
-        syndicateToken = SyndicateToken(
-            syndicateDeployer.DeployToken(
+        syndicateDeployerV1 = new SyndicateDeployerV1();
+        console.log("Deployer launched at: ", address(syndicateDeployerV1));
+        vm.startPrank(address(syndicateDeployerV1));
+        syndicateTokenV1 = SyndicateTokenV1(
+            syndicateDeployerV1.DeployToken(
                 owner,
                 initialSupply,
                 maxSupply,
@@ -35,22 +35,22 @@ contract SyndicateTokenTest is Test {
                 symbol
             )
         );
-        console.log("Contract deployed at: ", address(syndicateToken));
+        console.log("Contract deployed at: ", address(syndicateTokenV1));
         vm.stopPrank();
     }
 
-    function testContractAddress() public view returns (SyndicateToken) {
-        return syndicateToken;
+    function testContractAddress() public view returns (SyndicateTokenV1) {
+        return syndicateTokenV1;
     }
 
     function testTokenCreation() public view {
-        assertEq(syndicateToken.totalSupply(), initialSupply);
+        assertEq(syndicateTokenV1.totalSupply(), initialSupply);
     }
 
     function testCheckMaxSupply() public view {
         console.log("The initial supply is: ", initialSupply);
         console.log("The max supply is: ", maxSupply);
-        assertEq(maxSupply, syndicateToken.maxSupply());
+        assertEq(maxSupply, syndicateTokenV1.maxSupply());
     }
     //
     // function testTransfer() public {}
